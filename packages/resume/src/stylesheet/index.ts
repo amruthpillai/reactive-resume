8a7ef4b117b972d7637723b9f46d30b52b33cc62
Amruth Pillai <im.amruth@gmail.com>
2026-08-16 13:21:35 +0200

    refactor(stylesheet): remove projection fingerprints

--- packages/resume/src/stylesheet/index.ts
+++ packages/resume/src/stylesheet/index.ts
@@ -5,8 +5,6 @@ export type { SemanticCssCompilerDiagnosticCode } from "./diagnostics";
 export type { PropertyDefinition, PropertyRegistry } from "./registry/properties";
 export type { SemanticNodeDefinition, SemanticRegistry } from "./registry/semantic";
 export type { SystemVariableDefinition, SystemVariableRegistry } from "./registry/system-variables";
-export type { RenderDataProjection } from "./render-data";
-export type { RenderDataHashInput } from "./render-hash";
 export type { CompiledSelector, CompileSelectorResult, Specificity } from "./selector";
 export type { GeneratedStylesheet, GeneratedStylesheetBlock } from "./serialize";
 export type {
@@ -48,8 +46,6 @@ export {
 	TEMPLATE_PART_CHILD_KINDS_V1,
 } from "./registry/semantic";
 export { createSystemVariables, SYSTEM_VARIABLE_REGISTRY_V1 } from "./registry/system-variables";
-export { projectPublicRenderData, projectRenderData } from "./render-data";
-export { computeRenderDataHash } from "./render-hash";
 export { compileSelector, createSelectorMatcher, getSpecificity, matchesSelector } from "./selector";
 export { escapeCssComment, escapeCssString, serializeGeneratedStylesheet } from "./serialize";
 export { SUPPORTED_SEMANTIC_CSS_VERSIONS } from "./version";
